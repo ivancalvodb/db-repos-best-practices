cf48138b87fc4c0b26f1219fc5f772e629f3249b
ivan.calvo@databricks.com <ivan.calvo@databricks.com>
2023-07-20 22:42:12 +0000

    Updated comment

--- covid_analysis/transforms.py
+++ covid_analysis/transforms.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 
-# Filter by country code. If not specified, use "USA."
+# Filter by country code. If not specified, use "USA"
 def filter_country(pdf, country="USA"):
     pdf = pdf[pdf.iso_code == country]
     return pdf
